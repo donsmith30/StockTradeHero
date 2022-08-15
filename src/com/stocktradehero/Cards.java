d084bef32bf6e78a0a3a7e8c8c72a516bec83bc2
donsmith30 <donsmith30@hotmail.com>
2022-08-15 06:58:15 -0700

    Setting up notes for classes.

--- src/com/stocktradehero/Cards.java
+++ src/com/stocktradehero/Cards.java
@@ -10,6 +10,7 @@ package com.stocktradehero;
  * negativeEarnings     -- multiply stock by 0.8
  * badCompanyBehavior   -- multiply stock by 0.8
  * goodCompanyBehavior  -- multiply stock by 1.2
+ *
  * Chip shortage (negative for tech and industrial),
  * political situations, mergers and acquisitions, war, pandemic, interest rates, inflation,
  * food shortage (drought), climate emergency, oil shortage, bad company behavior, company earnings
