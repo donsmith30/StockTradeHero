24f8c297598693067bf7d0b269bcdfd766ea0339
Duncan <dbonacuse@gmail.com>
2022-08-17 12:52:51 -0700

    Added to StatusBoard and added StocksList.CSV, plus StatusBoardTest

--- src/com/stocktradehero/MarketEvent.java
+++ src/com/stocktradehero/MarketEvent.java
@@ -18,18 +18,22 @@ package com.stocktradehero;
 
 enum MarketEvent { //consider MarketEvent
 
-    CHIP_SHORTAGE(.7,1,1,1, "There is a chip shortage. Tech stocks are down 30%"),
-    ENVIRONMENTAL_SANCTIONS(1, 0.7, 1, 1, "New worldwide sanctions on mining operations. Precious Metal stocks are down 30%"),
-    CONSTRUCTION_MATERIAL(1, 1, 0.7, 1, "Global shortages in lumber and other raw construction material. Industrial stocks are down 30%"),
-    FOOD_SHORTAGE(1, 1, 1, 0.7, "There is a food shortage. Agricultural stocks are down 30%"),
+    CHIP_SHORTAGE(.7,1.0,1.0,1.0, "There is a chip shortage. Tech stocks are down 30%"),
+    METAVERSE_EXPANDING(1.3, 1.0, 1.0, 1.0, "Investments in the Metaverse have increased tenfold. Tech stocks up 30%."),
+    ENVIRONMENTAL_SANCTIONS(1.0, 0.7, 1.0, 1.0, "New worldwide sanctions on mining operations. Precious Metal stocks are down 30%"),
+    GOLD_STANDARD(1.0, 1.3, 1.0, 1.0, "The government has moved back to a Gold Standard for currency. Precious Metals are up 30%"),
+    CONSTRUCTION_MATERIAL_SHORTAGE(1.0, 1.0, 0.7, 1.0, "Global shortages in lumber and other raw construction material. Industrial stocks are down 30%"),
+    CONSTRUCTION_MATERIAL_SURPLUS(1.0, 1.0, 1.3, 1.0, "There is a global surplus of lumber and industrial supplies, and demand is high. Industrial stocks are up 30%"),
+    FOOD_SHORTAGE(1.0, 1.0, 1.0, 0.7, "There is a food shortage. Agricultural stocks are down 30%"),
+    HEALTHY_CLIMATE(1.0, 1.0, 1.0, 1.3, "Climate change is reversing. There are no droughts. Agriculture stocks are booming, and are up 30%"),
     POLITICAL_DILEMMA(0.9, 0.9, 0.9, 0.9, "There has been a political dilemma. The market is down 10%"),
     PANDEMIC(0.7, 0.7, 0.7, 0.7, "There is a new pandemic. The market is down 30%"),
     INFLATION_DOWN(1.1, 1.1, 1.1, 1.1, "Inflation is down. Market is up 10%"),
     INFLATION_UP(0.9, 0.9, 0.9, 0.9, "Inflation is up. Market is down 10%"),
-    POSITIVE_EARNINGS(1.3, 1.3, 1.3, 1.3, "Economy drives all stocks to see a positive earnings report. The market is up 30%"),
-    NEGATIVE_EARNINGS(.6, .6, .6,.6, "Economy drives all stocks to see a negative earnings report. The market is down 30%");
-//    GOOD_BEHAVIOR(1, 1, 1, 1, "This company is in the news for improving the world"), // Individual stock
-//    BAD_BEHAVIOR(1, 1, 1, 1, "This company is in the news for some egregious executive behavior"); // Individual stock
+    STIMULUS_PACKAGE(1.2, 1.2, 1.2, 1.2, "The government has signed a new stimulus bill. Investors are elated. The market is up 20%."),
+    INTEREST_RATES_LOWER(1.15, 1.15, 1.15, 1.15, "The Fed has lowered interest rates. Economic activity is bustling. Market is up 15%"),
+    POSITIVE_EARNINGS(1.3, 1.3, 1.3, 1.3, "Economy is thriving. Revenues are up for all stocks. The market is up 30%"),
+    NEGATIVE_EARNINGS(.6, .6, .6,.6, "Economy is dragging. Revenues are down for all stocks. The market is down 40%");
 
     private double xTech;
     private double xPreciousMetals;
